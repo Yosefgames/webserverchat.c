58a11a51b6ba3719aacc9cfee0e11988b7667d8c
Yosefgames <48890962+Yosefgames@users.noreply.github.com>
2019-08-06 01:10:30 +0100

    Create Mudita.TriggerButton.c

---
+++ Mudita.TriggerButton.c
@@ -0,0 +1,50 @@
+
+ // http://www.arduino.cc/mudita/Button/trigger
+ 
+ 
+#include <Bridge.h>
+#include <HttpClient.h>
+#include <SPI.h>
+#include <Ethernet.h>
+
+
+// constants won't change. They're used here to set pin numbers:
+const int buttonPin = 2;     // the number of the pushbutton pin
+const int ledPin =  13;      // the number of the LED pin 1 also drives the trigger push
+
+// variables will change:
+int buttonState = 0;         // variable for reading the pushbutton status
+
+void setup() {
+  // initialize the LED pin as an output:
+  pinMode(ledPin, OUTPUT);
+  // initialize the pushbutton pin as an input:
+  pinMode(buttonPin, INPUT);
+}
+
+void loop() {
+  // read the state of the pushbutton value:
+  buttonState = digitalRead(buttonPin);
+
+  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+  if (buttonState == HIGH) {
+    // turn LED on:
+    digitalWrite(ledPin, HIGH);
+	{
+		
+  while (Post.available()) { //Check connection status
+    HTTPClient Post;
+    Post.begin("http://lemontechnologies.net/Services/Service.svc/SetData"); 
+    Post.addHeader("operator", "text/plain");  
+    Post.POST("Key=hi&val=jagrut1");
+    String payload = Post.getString();
+    Post.end();
+  } 
+  delay(500);  
+}
+	
+  } else {
+    // turn LED off:
+    digitalWrite(ledPin, LOW);
+  }
+}
